9843c4da405b24c002d268dd6f4b27676702226e
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2022-10-20 23:24:18 -0300

    completado

--- src/main/java/vuelos/modelo/empleado/dao/DAOVuelosImpl.java
+++ src/main/java/vuelos/modelo/empleado/dao/DAOVuelosImpl.java
@@ -36,6 +36,7 @@ public class DAOVuelosImpl implements DAOVuelos {
 	@Override
 	public ArrayList<InstanciaVueloBean> recuperarVuelosDisponibles(Date fechaVuelo, UbicacionesBean origen,
 			UbicacionesBean destino) throws Exception {
+		
 		/**
 		 * TODO Debe retornar una lista de vuelos disponibles para ese día con origen y
 		 * destino según los parámetros. Debe propagar una excepción si hay algún error
@@ -45,7 +46,85 @@ public class DAOVuelosImpl implements DAOVuelos {
 		 * conexión establecida con el servidor de B.D. (inicializada en el constructor
 		 * DAOVuelosImpl(...)).
 		 */
-
+	        
+		funciona bien tu version pero no funciona con mi recuperarDetalle() no se porque !
+		
+		ArrayList<InstanciaVueloBean> resultado = new ArrayList<InstanciaVueloBean>();
+		
+		String fecha= Fechas.convertirDateAStringDB(fechaVuelo);
+		String sql = "select v.nro_vuelo,v.dia_sale,v.hora_sale,v.hora_llega,v.tiempo_estimado,v.modelo,v.fecha,"
+				+ "v.codigo_aero_sale, v.codigo_aero_llega, v.pais_sale, v.estado_sale, v.ciudad_sale, v.pais_llega, v.estado_llega,"
+				+ "v.ciudad_llega, v.codigo_aero_sale,v.codigo_aero_llega,v.pais_sale,v.estado_sale,v.ciudad_sale,v.pais_llega,v.estado_llega, v.ciudad_llega,"
+				+ " s.telefono as telefono_aero_sale, s.nombre as nombre_aero_sale, s.direccion as direccion_aero_sale,"
+				+ " l.telefono as telefono_aero_llega, l.nombre as nombre_aero_llega, l.direccion as direccion_aero_llega,"
+				+ " ul.huso as huso_llega, us.huso as huso_sale  from vuelos_disponibles AS v inner join aeropuertos as s on codigo_aero_sale = s.codigo"
+				+ " inner join aeropuertos l on codigo_aero_llega= l.codigo inner join"
+				+ " ubicaciones ul on pais_llega= ul.pais and estado_llega = ul.estado and ciudad_llega = ul.ciudad   inner join"
+				+ " ubicaciones us on pais_sale= us.pais and estado_sale = us.estado and ciudad_sale = us.ciudad  WHERE v.fecha= '"+fecha+"' and v.ciudad_sale= '"+origen.getCiudad()+"' and "
+					+ " v.estado_sale= '"+origen.getEstado()+"' and v.pais_sale= '"+origen.getPais()+"' and v.ciudad_llega= '"+destino.getCiudad()+"' and "
+							+ "v.estado_llega= '"+destino.getEstado()+"' and v.pais_llega= '"+destino.getPais()+"';"; 
+		try
+		{       
+			Statement s= conexion.createStatement();
+			ResultSet rs= s.executeQuery(sql);
+			while(rs.next()) {
+				AeropuertoBean abSalida= new AeropuertoBeanImpl();
+				AeropuertoBean abLlegada= new AeropuertoBeanImpl();
+				
+				abSalida.setCodigo(rs.getString("codigo_aero_sale"));
+				abSalida.setDireccion(rs.getString("direccion_aero_sale"));
+				abSalida.setNombre(rs.getString("nombre_aero_sale"));
+				abSalida.setTelefono(rs.getString("telefono_aero_sale"));
+	
+				abLlegada.setCodigo(rs.getString("codigo_aero_llega"));
+				abLlegada.setDireccion(rs.getString("direccion_aero_llega"));
+				abLlegada.setNombre(rs.getString("nombre_aero_llega"));
+				abLlegada.setTelefono(rs.getString("telefono_aero_llega"));
+				
+				UbicacionesBean ubSalida=  new UbicacionesBeanImpl();
+				UbicacionesBean ubLlegada=  new UbicacionesBeanImpl();
+				
+				ubSalida.setCiudad(rs.getString("ciudad_sale"));
+				ubSalida.setEstado(rs.getString("estado_sale"));
+				ubSalida.setHuso(rs.getInt("huso_sale"));
+				ubSalida.setPais(rs.getString("pais_sale"));
+				
+				ubLlegada.setCiudad(rs.getString("ciudad_llega"));
+				ubLlegada.setEstado(rs.getString("estado_llega"));
+				ubLlegada.setHuso(rs.getInt("huso_llega"));
+				ubLlegada.setPais(rs.getString("pais_llega"));
+				
+				abSalida.setUbicacion(ubSalida);
+				abLlegada.setUbicacion(ubLlegada);
+				
+				InstanciaVueloBean ins= new InstanciaVueloBeanImpl();
+				ins.setAeropuertoLlegada(abLlegada);
+				ins.setAeropuertoSalida(abSalida);
+				ins.setNroVuelo(rs.getString("nro_vuelo"));
+				ins.setFechaVuelo(rs.getDate("fecha"));
+				ins.setDiaSalida(rs.getString("dia_sale"));
+				ins.setHoraLlegada(rs.getTime("hora_llega"));
+				ins.setHoraSalida(rs.getTime("hora_sale"));
+				ins.setTiempoEstimado(rs.getTime("tiempo_estimado"));
+				ins.setModelo(rs.getString("modelo"));
+				
+				resultado.add(ins);
+			
+			}
+			s.close();
+			rs.close();
+		}
+		
+		catch (SQLException ex){
+		   logger.error("SQLException: " + ex.getMessage());
+		   logger.error("SQLState: " + ex.getSQLState());
+		   logger.error("VendorError: " + ex.getErrorCode());				   
+		}	
+	
+		return resultado; 
+		
+		
+		*/ 
 		String fechaVuelo_DBformatted = Fechas.convertirDateAStringDB(fechaVuelo);
 
 		String origen_ciudad = origen.getCiudad();
@@ -133,6 +212,7 @@ public class DAOVuelosImpl implements DAOVuelos {
 
 				// TODO Funciona!! Pero hay que corregir la vista.
 				toReturn.add(iv);
+				*/
 			}
 
 		} catch (Exception e) {
@@ -145,19 +225,32 @@ public class DAOVuelosImpl implements DAOVuelos {
 
 	@Override
 	public ArrayList<DetalleVueloBean> recuperarDetalleVuelo(InstanciaVueloBean vuelo) throws Exception {
-		/**
-		 * TODO Debe retornar una lista de clases, precios y asientos disponibles de
-		 * dicho vuelo. Debe propagar una excepción si hay algún error en la consulta.
-		 * 
-		 * Nota: para acceder a la B.D. utilice la propiedad "conexion" que ya tiene una
-		 * conexión establecida con el servidor de B.D. (inicializada en el constructor
-		 * DAOVuelosImpl(...)).
-		 */
-		// Datos estáticos de prueba. Quitar y reemplazar por código que recupera los
-		// datos reales.
 		ArrayList<DetalleVueloBean> resultado = DAOVuelosDatosPrueba.generarDetalles(vuelo);
-
-		return resultado;
-		// Fin datos estáticos de prueba.
+		String fechaVuelo= Fechas.convertirDateAStringDB(vuelo.getFechaVuelo());
+		String sql = "select clase, precio, asientos_disponibles from vuelos_disponibles where nro_vuelo= '"+vuelo.getNroVuelo()+"' and "
+				+ " fecha= '"+fechaVuelo+"' ;";
+		
+		try
+		{       
+			Statement s= conexion.createStatement();
+			ResultSet rs= s.executeQuery(sql);
+			while(rs.next()) {
+				DetalleVueloBeanImpl dvb= new DetalleVueloBeanImpl();
+				dvb.setAsientosDisponibles(rs.getInt("asientos_disponibles"));
+				dvb.setClase(rs.getString("clase"));
+				dvb.setPrecio(rs.getFloat("precio"));
+				dvb.setVuelo(vuelo);
+				resultado.add(dvb);
+			}
+		s.close();	
+		rs.close();
+		}
+		catch (SQLException ex){
+		   logger.error("SQLException: " + ex.getMessage());
+		   logger.error("SQLState: " + ex.getSQLState());
+		   logger.error("VendorError: " + ex.getErrorCode());				   
+		}
+		
+		return resultado; 
 	}
 }
