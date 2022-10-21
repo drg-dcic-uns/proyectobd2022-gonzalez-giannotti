ccfce369883b099a08430a170a6b09bc76fdb094
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2022-10-20 23:31:17 -0300

    Update DAOEmpleadoImpl.java

--- src/main/java/vuelos/modelo/empleado/dao/DAOEmpleadoImpl.java
+++ src/main/java/vuelos/modelo/empleado/dao/DAOEmpleadoImpl.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
