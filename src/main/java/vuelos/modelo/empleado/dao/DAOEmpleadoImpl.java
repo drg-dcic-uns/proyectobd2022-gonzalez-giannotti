4ce1280a78e5d9ce9d3192b84eddadfe275d0f7c
giulianogiannotti <71414041+giulianogiannotti@users.noreply.github.com>
2022-10-20 23:21:24 -0300

    completado

--- src/main/java/vuelos/modelo/empleado/dao/DAOEmpleadoImpl.java
+++ src/main/java/vuelos/modelo/empleado/dao/DAOEmpleadoImpl.java
@@ -26,37 +26,43 @@ public class DAOEmpleadoImpl implements DAOEmpleado {
 	public EmpleadoBean recuperarEmpleado(int legajo) throws Exception {
 		logger.info("recupera el empleado que corresponde al legajo {}.", legajo);
 
-		/**
-		 * TODO Debe recuperar de la B.D. los datos del empleado que corresponda al
-		 * legajo pasado como parámetro y devolver los datos en un objeto EmpleadoBean.
-		 * Si no existe el legajo deberá retornar null y si ocurre algun error deberá
-		 * generar y propagar una excepción.
-		 * 
-		 * Nota: para acceder a la B.D. utilice la propiedad "conexion" que ya tiene una
-		 * conexión establecida con el servidor de B.D. (inicializada en el constructor
-		 * DAOEmpleadoImpl(...)).
-		 */
-
-		/*
-		 * Datos estáticos de prueba. Quitar y reemplazar por código que recupera los
-		 * datos reales.
-		 */
+		String sql = "SELECT * FROM empleados WHERE legajo = " + legajo + ";";
 		EmpleadoBean empleado = null;
-
-		empleado = new EmpleadoBeanImpl();
-		empleado.setLegajo(9);
-		empleado.setApellido("ApEmp9");
-		empleado.setNombre("NomEmp9");
-		empleado.setTipoDocumento("DNI");
-		empleado.setNroDocumento(9);
-		empleado.setDireccion("DirEmp9");
-		empleado.setTelefono("999-9999");
-		empleado.setCargo("Empleado de Prestamos");
-		empleado.setPassword("45c48cce2e2d7fbdea1afc51c7c6ad26"); // md5(9);
-		empleado.setNroSucursal(7);
-
-		return empleado;
-		// Fin datos estáticos de prueba.
+		Statement select = null;
+		ResultSet rs = null;
+		
+		try {
+			select = conexion.createStatement();
+			rs = select.executeQuery(sql);
+			if(rs.next()) {
+				
+				// Si existe un empleado con el legajo indicado se crea el bean del empleado a retornar y se cargan los datos correspondientes
+				empleado = new EmpleadoBeanImpl();
+				empleado.setLegajo(rs.getInt("legajo"));
+				empleado.setApellido(rs.getString("apellido"));
+				empleado.setNombre(rs.getString("nombre"));
+				empleado.setTipoDocumento(rs.getString("doc_tipo"));
+				empleado.setNroDocumento(rs.getInt("doc_nro"));
+				empleado.setDireccion(rs.getString("direccion"));
+				empleado.setTelefono(rs.getString("telefono"));
+				empleado.setPassword(rs.getString("password"));
+			}
+			rs.close();
+			select.close();
+			return empleado;
+				
+		}
+		catch (SQLException ex)
+		{			
+			if(rs != null && !rs.isClosed())
+				rs.close();
+			if(select != null && !select.isClosed())
+				select.close();
+			logger.error("SQLException: " + ex.getMessage());
+			logger.error("SQLState: " + ex.getSQLState());
+			logger.error("VendorError: " + ex.getErrorCode());
+			throw new Exception("Error inesperado al consultar la B.D.");
+		}
 	}
 
 }
